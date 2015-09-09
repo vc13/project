c57f7e59070c35c861a5aa668f8bc69212aef766
Vaibhav Choudhary <vaibhavchoudhary13@hotmail.com>
2015-09-10 03:28:38 +0530

    Need to twik window size

--- test.py
+++ test.py
@@ -3,12 +3,74 @@ import wave
 from pydub import AudioSegment
 import pylab
 
+import numpy as np
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+from scipy.ndimage.filters import maximum_filter
+from scipy.ndimage.morphology import (generate_binary_structure,
+                                      iterate_structure, binary_erosion)
+
+
+######################################################################
+# Sampling rate, related to the Nyquist conditions, which affects
+# the range frequencies we can detect.
+DEFAULT_FS = 44100
+
+######################################################################
+# Size of the FFT window, affects frequency granularity
+DEFAULT_WINDOW_SIZE = 4096
+
+######################################################################
+# Ratio by which each sequential window overlaps the last and the
+# next window. Higher overlap will allow a higher granularity of offset
+# matching, but potentially more fingerprints.
+DEFAULT_OVERLAP_RATIO = 0.5
+
+######################################################################
+# Degree to which a fingerprint can be paired with its neighbors --
+# higher will cause more fingerprints, but potentially better accuracy.
+DEFAULT_FAN_VALUE = 15
+
+######################################################################
+# Minimum amplitude in spectrogram in order to be considered a peak.
+# This can be raised to reduce number of fingerprints, but can negatively
+# affect accuracy.
+DEFAULT_AMP_MIN = 10
+
+######################################################################
+# Number of cells around an amplitude peak in the spectrogram in order
+# for Dejavu to consider it a spectral peak. Higher values mean less
+# fingerprints and faster matching, but can potentially affect accuracy.
+PEAK_NEIGHBORHOOD_SIZE = 20
+
+######################################################################
+# Thresholds on how close or far fingerprints can be in time in order
+# to be paired as a fingerprint. If your max is too low, higher values of
+# DEFAULT_FAN_VALUE may not perform as expected.
+MIN_HASH_TIME_DELTA = 0
+MAX_HASH_TIME_DELTA = 200
+
+######################################################################
+# If True, will sort peaks temporally for fingerprinting;
+# not sorting will cut down number of fingerprints, but potentially
+# affect performance.
+PEAK_SORT = True
+
+######################################################################
+# Number of bits to throw away from the front of the SHA1 hash in the
+# fingerprint calculation. The more you throw away, the less storage, but
+# potentially higher collisions and misclassifications when identifying songs.
+FINGERPRINT_REDUCTION = 20
+
+
 def graph_spectrogram(wav_file):
     sound_info, frame_rate = get_wav_info(wav_file)
     pylab.figure(num=None, figsize=(19, 12))
     pylab.subplot(111)
     pylab.title('spectrogram of %r' % wav_file)
     pylab.specgram(sound_info, Fs=frame_rate)
+    i,j = peakdet(sound_info,1)
+    pylab.scatter(j,i)
     pylab.savefig('hello.png')
     print "Spectogram Generated"
 
@@ -21,9 +83,89 @@ def get_wav_info(wav_file):
     wav.close()
     return sound_info, frame_rate
 
-def init(mp3_file="",wav_file = 'hello.wav'):
+def secondFunction():
+    data, rate = get_wav_info("hello.wav")
+    fingerprint(data)
+
+def fingerprint(channel_samples, Fs=DEFAULT_FS,
+                wsize=DEFAULT_WINDOW_SIZE,
+                wratio=DEFAULT_OVERLAP_RATIO,
+                fan_value=DEFAULT_FAN_VALUE,
+                amp_min=DEFAULT_AMP_MIN):
+    """
+    FFT the channel, log transform output, find local maxima, then return
+    locally sensitive hashes.
+    """
+    # FFT the signal and extract frequency components
+    arr2D = mlab.specgram(
+        channel_samples,
+        NFFT=wsize,
+        Fs=Fs,
+        window=mlab.window_hanning,
+        noverlap=int(wsize * wratio))[0]
+    
+    # apply log transform since specgram() returns linear array
+    arr2D = 10 * np.log10(arr2D)
+    arr2D[arr2D == -np.inf] = 0  # replace infs with zeros
+    # generate_spectogram(arr2D)
+    # find local maxima
+    local_maxima = get_2D_peaks(arr2D, plot=True, amp_min=amp_min)
+
+    # return hashes
+    # return generate_hashes(local_maxima, fan_value=fan_value)
+
+
+def get_2D_peaks(arr2D, plot=False, amp_min=DEFAULT_AMP_MIN):
+    # http://docs.scipy.org/doc/scipy/reference/generated/scipy.ndimage.morphology.iterate_structure.html#scipy.ndimage.morphology.iterate_structure
+    struct = generate_binary_structure(2, 1)
+    neighborhood = iterate_structure(struct, PEAK_NEIGHBORHOOD_SIZE)
+
+    # find local maxima using our fliter shape
+    local_max = maximum_filter(arr2D, footprint=neighborhood) == arr2D
+    background = (arr2D == 0)
+    eroded_background = binary_erosion(background, structure=neighborhood,
+                                       border_value=1)
+
+    # Boolean mask of arr2D with True at peaks
+    detected_peaks = local_max - eroded_background
+
+    # extract peaks
+    amps = arr2D[detected_peaks]
+    j, i = np.where(detected_peaks)
+
+    # filter peaks
+    amps = amps.flatten()
+    peaks = zip(i, j, amps)
+    peaks_filtered = [x for x in peaks if x[2] > amp_min]  # freq, time, amp
+
+    # get indices for frequency and time
+    frequency_idx = [x[1] for x in peaks_filtered]
+    time_idx = [x[0] for x in peaks_filtered]
+
+    if plot:
+        # scatter of the peaks
+        # plt.figure(num=None,figsize=(19, 12))
+        fig, ax = plt.subplots()
+        ax.imshow(arr2D)
+        ax.scatter(time_idx, frequency_idx)
+        ax.set_xlabel('Time')
+        ax.set_ylabel('Frequency')
+        ax.set_title("Spectrogram")
+        fig.set_size_inches(19, 12)
+        plt.gca().invert_yaxis()
+        plt.savefig('hello.png',dpi=100,transparent=True)
+
+    # return zip(frequency_idx, time_idx
+
+
+
+def init(mp3_file="../dejavu/mp3/The-Lights-Galaxia--While-She-Sleeps.mp3",wav_file = 'hello.wav'):
     # AudioSegment.from_mp3(mp3_file).export(wav_file, format="wav")
-    graph_spectrogram(wav_file)
+    # graph_spectrogram(wav_file)
+    secondFunction()
+
+
+
 
 if __name__ == '__main__':
     init()
